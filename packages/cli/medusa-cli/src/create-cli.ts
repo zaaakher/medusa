zaaakher/medusa

commit 300ef8dbb90a699fdb7a53adffd5d95e0385263f

fix: consider --host flag and HOST env variable (#9912)

packages/cli/medusa-cli/src/create-cli.ts
@@ -290,8 +290,10 @@ function buildLocalCommands(cli, isLocalProject) {
           .option(`H`, {
             alias: `host`,
             type: `string`,
-            default: defaultHost,
-            describe: `Set host. Defaults to ${defaultHost}`,
+            default: process.env.HOST || defaultHost,
+            describe: process.env.HOST
+              ? `Set host. Defaults to ${process.env.HOST} (set by env.HOST) (otherwise defaults ${defaultHost})`
+              : `Set host. Defaults to ${defaultHost}`,
           })
           .option(`p`, {
             alias: `port`,
@@ -326,8 +328,10 @@ function buildLocalCommands(cli, isLocalProject) {
           .option(`H`, {
             alias: `host`,
             type: `string`,
-            default: defaultHost,
-            describe: `Set host. Defaults to ${defaultHost}`,
+            default: process.env.HOST || defaultHost,
+            describe: process.env.HOST
+              ? `Set host. Defaults to ${process.env.HOST} (set by env.HOST) (otherwise defaults ${defaultHost})`
+              : `Set host. Defaults to ${defaultHost}`,
           })
           .option(`p`, {
             alias: `port`,

packages/medusa/src/commands/start.ts
@@ -59,9 +59,11 @@ export async function registerInstrumentation(directory: string) {
 export var traceRequestHandler: (...args: any[]) => Promise<any> = void 0 as any
 
 function displayAdminUrl({
-  container,
+  host,
   port,
+  container,
 }: {
+  host: string
   port: string | number
   container: MedusaContainer
 }) {
@@ -82,16 +84,17 @@ function displayAdminUrl({
     return
   }
 
-  logger.info(`Admin URL → http://localhost:${port}${adminPath}`)
+  logger.info(`Admin URL → http://${host}:${port}${adminPath}`)
 }
 
 async function start(args: {
   directory: string
+  host?: string
   port?: number
   types?: boolean
   cluster?: number
 }) {
-  const { port = 9000, directory, types } = args
+  const { port = 9000, host = "localhost", directory, types } = args
 
   async function internalStart(generateTypes: boolean) {
     track("CLI_START")
@@ -136,9 +139,12 @@ async function start(args: {
 
       const serverActivity = logger.activity(`Creating server`)
       const server = GracefulShutdownServer.create(
-        http_.listen(port).on("listening", () => {
-          logger.success(serverActivity, `Server is ready on port: ${port}`)
-          displayAdminUrl({ container, port })
+        http_.listen(port, host).on("listening", () => {
+          logger.success(
+            serverActivity,
+            `Server is ready on http://${host}:${port}`
+          )
+          displayAdminUrl({ container, host, port })
           track("CLI_START_COMPLETED")
         })
       )

commit b0f416769fbca1cbc98b7c5cb22787970cc9e57f

fix: do not set default host to localhost (#9957)
Fixes bug introduced by https://github.com/medusajs/medusa/pull/9912

By default the Node.js server listens on the `0.0.0.0` host. However, the related PR changes the host to `localhost` and hence resulted in an unwanted breaking change. This PR reverts the default value assignment and let Node.js decide the host when not explicitly specified.

packages/cli/medusa-cli/src/create-cli.ts
@@ -18,8 +18,7 @@ const handlerP =
   }
 
 function buildLocalCommands(cli, isLocalProject) {
-  const defaultHost = `localhost`
-  const defaultPort = `9000`
+  const defaultPort = "9000"
   const directory = path.resolve(`.`)
 
   const projectInfo = { directory }
@@ -290,10 +289,10 @@ function buildLocalCommands(cli, isLocalProject) {
           .option(`H`, {
             alias: `host`,
             type: `string`,
-            default: process.env.HOST || defaultHost,
+            default: process.env.HOST,
             describe: process.env.HOST
-              ? `Set host. Defaults to ${process.env.HOST} (set by env.HOST) (otherwise defaults ${defaultHost})`
-              : `Set host. Defaults to ${defaultHost}`,
+              ? `Set host. Defaults to ${process.env.HOST} (set by env.HOST)`
+              : "",
           })
           .option(`p`, {
             alias: `port`,
@@ -328,10 +327,10 @@ function buildLocalCommands(cli, isLocalProject) {
           .option(`H`, {
             alias: `host`,
             type: `string`,
-            default: process.env.HOST || defaultHost,
+            default: process.env.HOST,
             describe: process.env.HOST
-              ? `Set host. Defaults to ${process.env.HOST} (set by env.HOST) (otherwise defaults ${defaultHost})`
-              : `Set host. Defaults to ${defaultHost}`,
+              ? `Set host. Defaults to ${process.env.HOST} (set by env.HOST)`
+              : ``,
           })
           .option(`p`, {
             alias: `port`,

packages/medusa/src/commands/start.ts
@@ -63,7 +63,7 @@ function displayAdminUrl({
   port,
   container,
 }: {
-  host: string
+  host?: string
   port: string | number
   container: MedusaContainer
 }) {
@@ -84,7 +84,7 @@ function displayAdminUrl({
     return
   }
 
-  logger.info(`Admin URL → http://${host}:${port}${adminPath}`)
+  logger.info(`Admin URL → http://${host || "localhost"}:${port}${adminPath}`)
 }
 
 async function start(args: {
@@ -94,7 +94,7 @@ async function start(args: {
   types?: boolean
   cluster?: number
 }) {
-  const { port = 9000, host = "localhost", directory, types } = args
+  const { port = 9000, host, directory, types } = args
 
   async function internalStart(generateTypes: boolean) {
     track("CLI_START")
@@ -142,7 +142,7 @@ async function start(args: {
         http_.listen(port, host).on("listening", () => {
           logger.success(
             serverActivity,
-            `Server is ready on http://${host}:${port}`
+            `Server is ready on http://${host || "localhost"}:${port}`
           )
           displayAdminUrl({ container, host, port })
           track("CLI_START_COMPLETED")