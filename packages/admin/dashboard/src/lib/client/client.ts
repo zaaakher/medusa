cdd7e9353b7d3815e8f3f007049a6fe29c213b3d
Kasper Fabricius Kristensen <45367945+kasperkristensen@users.noreply.github.com>
2024-11-07 15:21:21 +0100

    cleanp

--- packages/admin/dashboard/src/lib/client/client.ts
+++ packages/admin/dashboard/src/lib/client/client.ts
@@ -13,39 +13,3 @@ export const sdk = new Medusa({
 if (typeof window !== "undefined") {
   ;(window as any).__sdk = sdk
 }
-
-sdk.admin.product.create({
-  title: "Medusa Shirt",
-  options: [
-    {
-      title: "Color",
-      values: [
-        "Red",
-        "Blue",
-        "Green",
-        "Yellow",
-        "Purple",
-        "Orange",
-        "Pink",
-        "Gray",
-        "Brown",
-        "Navy",
-        "Teal",
-      ],
-    },
-  ],
-  variants: [
-    {
-      title: "Red Shirt",
-      options: {
-        Color: "Red",
-      },
-      prices: [
-        {
-          amount: 1000,
-          currency_code: "eur",
-        },
-      ],
-    },
-  ],
-})
