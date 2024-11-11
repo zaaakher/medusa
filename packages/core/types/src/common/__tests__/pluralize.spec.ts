e228088f8d75796b2f4eb91f58a411066b386e17
Harminder Virk <virk.officials@gmail.com>
2024-11-11 17:18:23 +0530

    fix: pluralize type helper to account for uncountable nouns and special rules (#10011)

---
+++ packages/core/types/src/common/__tests__/pluralize.spec.ts
@@ -0,0 +1,44 @@
+import { expectTypeOf } from "expect-type"
+import { Pluralize } from "../common"
+
+describe("Pluralize", () => {
+  test("pluralize uncountable nouns", () => {
+    expectTypeOf<Pluralize<"media">>().toEqualTypeOf<"media">()
+    expectTypeOf<Pluralize<"Media">>().toEqualTypeOf<"Media">()
+    expectTypeOf<Pluralize<"you">>().toEqualTypeOf<"you">()
+    expectTypeOf<Pluralize<"sheep">>().toEqualTypeOf<"sheep">()
+    expectTypeOf<Pluralize<"series">>().toEqualTypeOf<"series">()
+    expectTypeOf<Pluralize<"species">>().toEqualTypeOf<"species">()
+    expectTypeOf<Pluralize<"deer">>().toEqualTypeOf<"deer">()
+  })
+
+  test("pluralize words ending with fe", () => {
+    expectTypeOf<Pluralize<"wife">>().toEqualTypeOf<"wives">()
+    expectTypeOf<Pluralize<"knife">>().toEqualTypeOf<"knives">()
+  })
+
+  test("pluralize words ending with o", () => {
+    expectTypeOf<Pluralize<"hero">>().toEqualTypeOf<"heroes">()
+  })
+
+  test("pluralize words ending with ch", () => {
+    expectTypeOf<Pluralize<"watch">>().toEqualTypeOf<"watches">()
+  })
+
+  test("pluralize words ending with z", () => {
+    expectTypeOf<Pluralize<"fiz">>().toEqualTypeOf<"fizes">()
+  })
+
+  test("pluralize words ending with y", () => {
+    expectTypeOf<Pluralize<"puppy">>().toEqualTypeOf<"puppies">()
+  })
+
+  test("pluralize words with special rules", () => {
+    expectTypeOf<Pluralize<"person">>().toEqualTypeOf<"people">()
+    expectTypeOf<Pluralize<"child">>().toEqualTypeOf<"children">()
+    expectTypeOf<Pluralize<"man">>().toEqualTypeOf<"men">()
+    expectTypeOf<Pluralize<"criterion">>().toEqualTypeOf<"criteria">()
+    expectTypeOf<Pluralize<"tooth">>().toEqualTypeOf<"teeth">()
+    expectTypeOf<Pluralize<"foot">>().toEqualTypeOf<"feet">()
+  })
+})
