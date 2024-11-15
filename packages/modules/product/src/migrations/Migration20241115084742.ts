4c2267388ce5d92178f0074660e986f9a3c195eb
Kasper Fabricius Kristensen <45367945+kasperkristensen@users.noreply.github.com>
2024-11-15 10:31:25 +0100

    fix snapshot

--- packages/modules/product/src/migrations/Migration20241115084742.ts
+++ packages/modules/product/src/migrations/Migration20241115084742.ts
@@ -2,10 +2,10 @@ import { Migration } from '@mikro-orm/migrations';
 
 export class Migration20241115084742 extends Migration {
   async up(): Promise<void> {
-    this.addSql('ALTER TABLE "product_images" ADD COLUMN "rank" integer DEFAULT 0 NOT NULL;')
+    this.addSql('alter table "product_images" add column "rank" integer default 0 not null;')
   }
 
   async down(): Promise<void> {
-    this.addSql('ALTER TABLE "product_images" DROP COLUMN "rank";')
+    this.addSql('alter table "product_images" drop column "rank";')
   }
 }
