78e68a0bb635060fb2d335bb6dbda566afc06a17
Shahed Nasser <shahednasser@gmail.com>
2024-11-22 18:42:49 +0200

    docs: fix base path not added to hash links (#10235)

---
+++ www/packages/docs-ui/src/hooks/use-heading-url/index.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import { usePathname } from "next/navigation"
+import { useIsBrowser, useSiteConfig } from "../../providers"
+import { useMemo } from "react"
+
+type useHeadingUrlProps = {
+  id: string
+}
+
+export const useHeadingUrl = ({ id }: useHeadingUrlProps) => {
+  const { isBrowser } = useIsBrowser()
+  const {
+    config: { basePath },
+  } = useSiteConfig()
+  const pathname = usePathname()
+  const headingUrl = useMemo(() => {
+    const hash = `#${id}`
+    if (!isBrowser) {
+      return hash
+    }
+
+    const url = `${window.location.origin}${basePath}${pathname}`.replace(
+      /\/$/,
+      ""
+    )
+
+    return `${url}${hash}`
+  }, [id, isBrowser, pathname])
+
+  return headingUrl
+}
