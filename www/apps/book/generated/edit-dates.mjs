065146c19d9694f2d69ae331aedc5e36f78329b4
Shahed Nasser <shahednasser@gmail.com>
2024-11-29 09:24:29 +0200

    docs: document permanentFailure (#10356)

--- www/apps/book/generated/edit-dates.mjs
+++ www/apps/book/generated/edit-dates.mjs
@@ -46,7 +46,7 @@ export const generatedEditDates = {
   "app/learn/advanced-development/modules/remote-query/page.mdx": "2024-07-21T21:20:24+02:00",
   "app/learn/advanced-development/modules/options/page.mdx": "2024-10-16T08:49:27.162Z",
   "app/learn/advanced-development/data-models/relationships/page.mdx": "2024-11-27T16:05:55.995Z",
-  "app/learn/advanced-development/workflows/compensation-function/page.mdx": "2024-09-30T08:43:53.128Z",
+  "app/learn/advanced-development/workflows/compensation-function/page.mdx": "2024-11-28T14:05:29.691Z",
   "app/learn/advanced-development/modules/service-factory/page.mdx": "2024-09-30T08:43:53.127Z",
   "app/learn/advanced-development/data-models/primary-key/page.mdx": "2024-09-30T08:43:53.123Z",
   "app/learn/advanced-development/modules/module-links/page.mdx": "2024-09-30T08:43:53.126Z",
