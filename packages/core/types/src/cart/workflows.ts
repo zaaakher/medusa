7e646e4735f8c26fca9ff7d7c91aabeec7a978ca
Riqwan Thamir <rmthamir@gmail.com>
2024-11-29 11:55:29 +0100

    fix(types): removes customer_id from update cart workflow type

--- packages/core/types/src/cart/workflows.ts
+++ packages/core/types/src/cart/workflows.ts
@@ -88,7 +88,6 @@ export interface UpdateCartWorkflowInputDTO {
   id: string
   promo_codes?: string[]
   region_id?: string
-  customer_id?: string | null
   sales_channel_id?: string | null
   email?: string | null
   currency_code?: string
