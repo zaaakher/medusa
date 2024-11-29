e083b5d5ea2ca1962bd2eddb824f2e3f3f388cf3
Kasper Fabricius Kristensen <45367945+kasperkristensen@users.noreply.github.com>
2024-11-29 14:29:31 +0100

    save column changes

--- packages/admin/dashboard/src/routes/locations/common/components/shipping-option-price-provider/shipping-option-price-context.tsx
+++ packages/admin/dashboard/src/routes/locations/common/components/shipping-option-price-provider/shipping-option-price-context.tsx
@@ -3,6 +3,7 @@ import { ConditionalPriceInfo } from "../../types"
 
 type ShippingOptionPriceContextType = {
   onOpenConditionalPricesModal: (info: ConditionalPriceInfo) => void
+  onCloseConditionalPricesModal: () => void
 }
 
 export const ShippingOptionPriceContext =
