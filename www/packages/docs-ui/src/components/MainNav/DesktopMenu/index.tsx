0e2f608db8eaeaa4b155e0d50ef1121dadd1b019
ahrbil <ahrbil.me@gmail.com>
2024-12-31 10:05:00 +0100

    docs: update desktop nav menu item title (#10125)
    Update menu item title to reflect current state of the desktop sidebar

--- www/packages/docs-ui/src/components/MainNav/DesktopMenu/index.tsx
+++ www/packages/docs-ui/src/components/MainNav/DesktopMenu/index.tsx
@@ -22,7 +22,8 @@ import { MenuItem } from "types"
 
 export const MainNavDesktopMenu = () => {
   const [isOpen, setIsOpen] = useState(false)
-  const { setDesktopSidebarOpen, isSidebarShown } = useSidebar()
+  const { setDesktopSidebarOpen, isSidebarShown, desktopSidebarOpen } =
+    useSidebar()
   const ref = useRef<HTMLDivElement>(null)
 
   useClickOutside({
@@ -65,7 +66,7 @@ export const MainNavDesktopMenu = () => {
         },
         {
           type: "action",
-          title: "Hide Sidebar",
+          title: desktopSidebarOpen ? "Hide Sidebar" : "Show Sidebar",
           icon: <SidebarLeft />,
           shortcut: `${getOsShortcut()}\\`,
           action: () => {
@@ -87,7 +88,7 @@ export const MainNavDesktopMenu = () => {
     )
 
     return items
-  }, [isSidebarShown])
+  }, [isSidebarShown, desktopSidebarOpen])
 
   return (
     <div
