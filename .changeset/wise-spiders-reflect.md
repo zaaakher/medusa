909b685961ec43b35d74db2b158769d6ef33d5cf
Paul Nodet <paul.nodet@gmail.com>
2025-01-22 14:48:26 +0100

    refactor(ui-preset): replace defaultTheme import (#11041)
    Removing the import allows future compatibilty with tailwind v4 where it won't be exported anymore. Fix #11040

---
+++ .changeset/wise-spiders-reflect.md
@@ -0,0 +1,5 @@
+---
+"@medusajs/ui-preset": minor
+---
+
+Remove tailwindcss/defaultTheme import
--- packages/design-system/ui-preset/src/constants.ts
+++ packages/design-system/ui-preset/src/constants.ts
@@ -1,4 +1,32 @@
-import { fontFamily } from "tailwindcss/defaultTheme"
+const tailwindFontFamily = {
+  sans: [
+    "ui-sans-serif",
+    "system-ui",
+    "sans-serif",
+    '"Apple Color Emoji"',
+    '"Segoe UI Emoji"',
+    '"Segoe UI Symbol"',
+    '"Noto Color Emoji"',
+  ],
+  serif: [
+    "ui-serif",
+    "Georgia",
+    "Cambria",
+    '"Times New Roman"',
+    "Times",
+    "serif",
+  ],
+  mono: [
+    "ui-monospace",
+    "SFMono-Regular",
+    "Menlo",
+    "Monaco",
+    "Consolas",
+    '"Liberation Mono"',
+    '"Courier New"',
+    "monospace",
+  ],
+}
 
-export const FONT_FAMILY_SANS = ["Inter", ...fontFamily.sans]
-export const FONT_FAMILY_MONO = ["Roboto Mono", ...fontFamily.mono]
+export const FONT_FAMILY_SANS = ["Inter", ...tailwindFontFamily.sans]
+export const FONT_FAMILY_MONO = ["Roboto Mono", ...tailwindFontFamily.mono]
